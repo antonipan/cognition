a550211ae5b1e41e31fdca060c46e1aa7c988806
antonio <a.s.panferov@mail.ru>
2024-03-15 12:39:07 +0300

    begann to write tests dor service

--- src/test/java/ru/antonio/cognition/repositories/QuestionnaireDaoTest.java
+++ src/test/java/ru/antonio/cognition/repositories/QuestionnaireDaoTest.java
@@ -1,4 +1,9 @@
 package ru.antonio.cognition.repositories;
 
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class QuestionnaireDaoTest {
+
+    // TODO: 15.03.2024 Не хватает знаний, чтобы написать тесты для слоя репозитория.  
 }
